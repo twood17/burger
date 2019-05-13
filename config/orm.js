d84d232663b93a9f793ac16c5b2903a15452d617
Ty Wood <twood06@gmail.com>
2019-05-13 14:07:12 -0400

    connects to localhost and to mysql

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+
+};
+
+
+//Exporting to ../models/burger.js
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,9 @@
+var orm = require("../config/orm.js")
+
+var burger = {
+    
+}
+
+
+//Exporting to ../controllers/burger_controller.js
+module.exports = burger
