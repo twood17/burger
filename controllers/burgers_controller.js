319d33cbe22ae50d875cc1494b2beca2d70ef283
Ty Wood <twood06@gmail.com>
2019-05-14 14:16:40 -0400

    handlebars work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,15 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+var app = express();
 
+var path = require("path");
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+router.get("/", function (req, res){
+    res.render("index")
+})
 
 
 //Exporting to ../server.js
