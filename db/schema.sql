2a7189f9c738068fe5830cb4756717bac9f1e74a
Ty Wood <twood06@gmail.com>
2019-05-16 12:50:07 -0400

    can add burgers!

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE DATABASE burgers_db;
 CREATE TABLE burgers(
     id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR (255),
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0),
     PRIMARY KEY (id)
 );
