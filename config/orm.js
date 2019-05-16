f3f3177db593250016944063c2548ccfa0624bf5
Ty Wood <twood06@gmail.com>
2019-05-16 13:03:38 -0400

    delete function working

--- config/orm.js
+++ config/orm.js
@@ -81,6 +81,20 @@ var orm = {
         });
     },
 
+    delete: function (table, condition, cb){
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+    }
+
 };
 
 //Exporting to ../models/burger.js
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -21,7 +21,7 @@ $(function() {
       });
 
 
-      $(".change-devour").on("click", function(event) {
+      $(".changeDevour").on("click", function(event) {
         var id = $(this).data("id");
         var newDevour = $(this).data("newdevour");
     
@@ -41,4 +41,17 @@ $(function() {
           }
         );
       });
+
+      $(".deleteBurger").on("click", function (event) {
+          var id = $(this).data("id");
+
+          $.ajax("/api/burgers/"+ id, {
+              type: "DELETE",
+          }).then(
+              function() {
+                  console.log("Burger Deleted");
+                  location.reload()
+              }
+          )
+      })
 });
